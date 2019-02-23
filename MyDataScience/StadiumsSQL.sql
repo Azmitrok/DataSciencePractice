746d3453d60b4fa3a682d61d0ba78cc6f0d9f479
Azmitrok <azmitrok@gmail.com>
2019-02-23 14:39:32 +0300

    Create StadiumsSQL.sql

---
+++ MyDataScience/StadiumsSQL.sql
@@ -0,0 +1,5 @@
+﻿select StadId, matchdate
+, (select DATEDIFF(DAY,  max(MatchDate),s1.MatchDate) from Stadium s2 
+where s2.StadId = s1.StadId and s2.Id!=s1.Id and  s2.MatchDate <= s1.MatchDate )
+from Stadium s1
+--where s1.StadId=1 order by s1.MatchDate
